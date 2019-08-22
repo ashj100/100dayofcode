b2b71ac85be92b686244f432cb950ae3376300e1
ashj100 <ash.alj15@gmail.com>
2019-08-22 22:55:56 +0300

    second day

---
+++ day2.py
@@ -0,0 +1,2 @@
+#this is a comment 
+print("you can't see my comments :P ")
---
+++ day2/day2.py
@@ -0,0 +1,16 @@
+#you can't see my comments
+print   (" Hi ")
+
+print(" hello girl") #don't be stupid
+
+#print("heeeeeeelllllo anyone is here?") 
+print("heeeeeeelllllo anyone is here?")
+
+#احب نفسي
+# احبني
+#احب نفسي اقين
+print("my name is Asalah")
+
+"""
+باااااااااااااااااي
+"""
