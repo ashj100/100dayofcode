f5377db1cc2b46f4de3318b6a36aadc020bf2fcc
ashj100 <ash.alj15@gmail.com>
2019-08-23 22:28:34 +0300

    Third day

---
+++ day3.py
@@ -0,0 +1,20 @@
+x=5
+y=2
+print(x+y)
+
+z="Asalah"
+print(z)
+
+a,b,c= 10,5,8
+print(a,b,c)
+
+d = f = e = "happy"
+print(d,e,f)
+
+g="ugly"
+print("java is "+g )
+
+h  = "you are "
+i = "a geek "
+j = h+i
+print(j)
