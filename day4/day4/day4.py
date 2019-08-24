132a42047d70558c612411a727aee18db9753e55
ashj100 <ash.alj15@gmail.com>
2019-08-24 22:54:16 +0300

    day4

---
+++ day4/day4/day4.py
@@ -0,0 +1,25 @@
+x=101 #int 
+y=120.5 #float
+z=9j #complex
+
+# لعرض نوع المتغير نكتب type
+
+print(type(x))
+print (type(y))
+print (type(z))
+
+# الاعداد المرفوعة لقوى العشرة يوضع بعد العدد حرف " اي " كرمز للأس وبعدا توضع قيمة الأس 
+
+h= 35e4
+print (type(h))
+
+#العدد العشوائي 
+import random 
+print(random.randrange(1,10))
+
+#بــــــــــدايـــــة اليوم الخامس 
+
+x,y,z= " apple "," orange " , " limon " 
+basket= x+y+z
+print(basket)
+
